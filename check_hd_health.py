495a82b70ee5e8c012eafe755b95887deeaf9c7c
root <root@9545be286232.(none)>
2014-11-22 12:38:50 +0000

    ADD check_hd_health.py

---
+++ check_hd_health.py
@@ -0,0 +1,29 @@
+import os
+import time
+import commands
+
+from pymongo import MongoClient
+from ConfigParser import SafeConfigParser
+
+result=""
+
+client = MongoClient()
+database = client['mydb']
+collection = database['hdhealth']
+
+parser = SafeConfigParser()
+parser.read('insider.conf')
+
+hd = parser.get('HD_DEVICE', 'hd')
+cmd = "smartctl -q errorsonly -H -A -a " + hd
+
+print commands.getoutput(cmd)
+
+if  commands.getoutput(cmd) == result :
+	print "good"
+	item = { "hd" : hd, "status" : "good"}
+	collection.insert(item)
+else:
+	print "better watch out"
+	item = { "hd": hd, "status" : "bad" }
+        collection.insert(item)
